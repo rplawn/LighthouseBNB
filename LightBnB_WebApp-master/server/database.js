b19ddc0fc50aecf99b8a00329a051e92da8a7d1a
roop <rplawn@icloud.com>
2022-06-10 20:46:56 -0700

    lint clean up

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -68,14 +68,14 @@ exports.addUser = addUser;
  */
 const getAllReservations = function (guest_id, limit = 10) {
   const queryString = `
-SELECT reservations.*, properties.*, avg(rating) as average_rating
-FROM reservations
-JOIN properties ON reservations.property_id = properties.id
-JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE reservations.guest_id = $1
-GROUP BY properties.id, reservations.id
-ORDER BY reservations.start_date
-LIMIT $2;
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
   `;
 
   const params = [guest_id, limit];
